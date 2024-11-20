2b40efbf723e27fedbcccb1848af51d8ff6ed7f5
samuelj1323 <samuelj1323@gmail.com>
2024-11-19 21:11:03 -0500

    Fixing mobile navigation

--- src/Components/HeaderBar/HeaderBar.jsx
+++ src/Components/HeaderBar/HeaderBar.jsx
@@ -6,6 +6,8 @@ import {
   Button,
   useTheme,
   ButtonGroup,
+  Menu,
+  MenuItem,
   useMediaQuery,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
@@ -13,6 +15,12 @@ const HeaderBar = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClose = (page) => {
+    typeof page === "string" && navigateToPage(page);
+    setAnchorEl(null);
+  };
   const appBarStyle = {
     backgroundColor: theme.palette.background.default,
     minWidth: "100%",
@@ -20,7 +28,6 @@ const HeaderBar = () => {
     color: theme.palette.text.primary,
     borderBottom: "0", // Explicitly remove any borders
   };
-
   const toolbarStyle = {
     padding: "4px 16px",
     width: "auto",
@@ -40,6 +47,60 @@ const HeaderBar = () => {
     navigate(page);
   };
 
+  const menuItems = (
+    <ButtonGroup variant="text">
+      <Button style={buttonStyle} onClick={() => navigateToPage("/about")}>
+        About
+      </Button>
+      <Button style={buttonStyle} onClick={() => navigateToPage("/background")}>
+        Background
+      </Button>
+      <Button style={buttonStyle} onClick={() => navigateToPage("/projects")}>
+        Projects
+      </Button>
+      <Button style={buttonStyle} onClick={() => navigateToPage("/contact")}>
+        Contact
+      </Button>
+    </ButtonGroup>
+  );
+
+  const smallScreenMenu = (
+    <Menu
+      id="basic-menu"
+      anchorEl={anchorEl}
+      open={open}
+      onClose={handleClose}
+      MenuListProps={{
+        "aria-labelledby": "basic-button",
+      }}
+    >
+      <MenuItem
+        style={{ color: "black" }}
+        onClick={() => handleClose("/about")}
+      >
+        About
+      </MenuItem>
+      <MenuItem
+        style={{ color: "black" }}
+        onClick={() => handleClose("/background")}
+      >
+        Background
+      </MenuItem>
+      <MenuItem
+        style={{ color: "black" }}
+        onClick={() => handleClose("/projects")}
+      >
+        Projects
+      </MenuItem>
+      <MenuItem
+        style={{ color: "black" }}
+        onClick={() => handleClose("/contact")}
+      >
+        Contact
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <div>
       <AppBar
@@ -57,33 +118,19 @@ const HeaderBar = () => {
             Samuel Mahan
           </Typography>
 
-          {!isSmallScreen && (
-            <ButtonGroup variant="text">
+          {!isSmallScreen ? (
+            menuItems
+          ) : (
+            <div>
               <Button
-                style={buttonStyle}
-                onClick={() => navigateToPage("/about")}
+                onClick={(event) => {
+                  setAnchorEl(event.currentTarget);
+                }}
               >
-                About
+                MENU
               </Button>
-              <Button
-                style={buttonStyle}
-                onClick={() => navigateToPage("/background")}
-              >
-                Background
-              </Button>
-              <Button
-                style={buttonStyle}
-                onClick={() => navigateToPage("/projects")}
-              >
-                Projects
-              </Button>
-              <Button
-                style={buttonStyle}
-                onClick={() => navigateToPage("/contact")}
-              >
-                Contact
-              </Button>
-            </ButtonGroup>
+              {smallScreenMenu}
+            </div>
           )}
         </Toolbar>
       </AppBar>
