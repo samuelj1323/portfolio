3a3b264ee60ae9c4735f5fdec3cfb7effcc0db4f
samuelj1323 <samuelj1323@gmail.com>
2026-09-04 07:35:23 -0400

    Fix nav links: use react-router Link so HashRouter URLs stay under /portfolio/#/

--- src/lib/components/nav-item/nav-item.tsx
+++ src/lib/components/nav-item/nav-item.tsx
@@ -1,25 +1,20 @@
 import type { Route } from "$utils/types";
 import { routeMappings } from "$utils/constants";
-import useTypedNavigate from "$hooks/useTypedNavigate";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 
 type NavItemProps = {
   route: Route;
 };
 
 const NavItem = ({ route }: NavItemProps) => {
-  const nav = useTypedNavigate();
   const { pathname } = useLocation();
   const name = routeMappings[route];
   const isActive = pathname === route;
-  const handleNavigation = () => {
-    nav(route);
-  };
   return (
     <span>
-      <a onClick={handleNavigation} href={route} aria-current={isActive ? "page" : undefined}>
+      <Link to={route} aria-current={isActive ? "page" : undefined}>
         {name}
-      </a>
+      </Link>
     </span>
   );
 };
